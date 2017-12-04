e4cc30f670f5e5e5fb4a6e1f9b741c7d7f2653db
Hoang Trung <trung.phan150041@vnuk.edu.vn>
2017-12-04 19:45:12 +0700

    Timer added

--- app/src/main/java/com/example/dande/alarm/TimeFragment.java
+++ app/src/main/java/com/example/dande/alarm/TimeFragment.java
@@ -1,11 +1,195 @@
 package com.example.dande.alarm;
 
+import android.content.Context;
+import android.os.Bundle;
+import android.os.CountDownTimer;
+import android.os.Handler;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.concurrent.TimeUnit;
+
 /**
  * Created by dande on 11/28/2017.
  */
 
 public class TimeFragment extends MyFragment{
     public TimeFragment(){
-        setName("Đếm ngược");
+        setName("Đếm ngược");}
+
+        ProgressBar progressBarCircle;
+        EditText editTextMinute;
+        TextView textViewTime;
+        Handler customHandler = new Handler();
+        ImageView imageViewReset, imageViewStartStop;
+        CountDownTimer countDownTimer;
+        TimerStatus timerStatus;
+
+
+    private int timeCountInMilliSeconds = 1 * 60000;
+
+
+    private enum TimerStatus{
+        STARTED,
+        STOPPED
     }
+
+    private void reset(){
+        stopCountDownTimer();
+    }
+
+    private void setTimerValues() {
+        int time = 0;
+        if (!editTextMinute.getText().toString().isEmpty()) {
+            // fetching value from edit text and type cast to integer
+            time = Integer.parseInt(editTextMinute.getText().toString().trim());
+        } else {
+            // toast message to fill edit text
+            Toast.makeText(getContext(), getString(R.string.message_minutes), Toast.LENGTH_LONG).show();
+        }
+        // assigning values after converting to milliseconds
+        timeCountInMilliSeconds = time * 60 * 1000;
+    }
+
+    private void startStop() {
+        if (timerStatus == TimerStatus.STOPPED) {
+
+            // call to initialize the timer values
+            setTimerValues();
+            // call to initialize the progress bar values
+            setProgressBarValues();
+            // showing the reset icon
+            imageViewReset.setVisibility(View.VISIBLE);
+            // changing play icon to stop icon
+            imageViewStartStop.setImageResource(R.drawable.icon_stop);
+            // making edit text not editable
+            editTextMinute.setEnabled(false);
+            // changing the timer status to started
+            timerStatus = TimerStatus.STARTED;
+            // call to start the count down timer
+            startCountDownTimer();
+
+        } else {
+
+            // hiding the reset icon
+            imageViewReset.setVisibility(View.GONE);
+            // changing stop icon to start icon
+            imageViewStartStop.setImageResource(R.drawable.icon_start);
+            // making edit text editable
+            editTextMinute.setEnabled(true);
+            // changing the timer status to stopped
+            timerStatus = TimerStatus.STOPPED;
+            stopCountDownTimer();
+
+        }
+
+    }
+
+    private String hmsTimeFormatter(long milliSeconds) {
+
+        String hms = String.format("%02d:%02d:%02d",
+                TimeUnit.MILLISECONDS.toHours(milliSeconds),
+                TimeUnit.MILLISECONDS.toMinutes(milliSeconds) - TimeUnit.HOURS.toMinutes(TimeUnit.MILLISECONDS.toHours(milliSeconds)),
+                TimeUnit.MILLISECONDS.toSeconds(milliSeconds) - TimeUnit.MINUTES.toSeconds(TimeUnit.MILLISECONDS.toMinutes(milliSeconds)));
+
+        return hms;
+
+
+    }
+
+    private void setProgressBarValues() {
+
+        progressBarCircle.setMax((int) timeCountInMilliSeconds / 1000);
+        progressBarCircle.setProgress((int) timeCountInMilliSeconds / 1000);
+    }
+
+    private void stopCountDownTimer() {
+        countDownTimer = new CountDownTimer(timeCountInMilliSeconds, 1000) {
+            @Override
+            public void onTick(long l) {
+
+            }
+
+            @Override
+            public void onFinish() {
+                countDownTimer.cancel();
+            }
+        };
+
+    }
+
+    private void startCountDownTimer() {
+
+        countDownTimer = new CountDownTimer(timeCountInMilliSeconds, 1000) {
+            @Override
+            public void onTick(long millisUntilFinished) {
+
+                textViewTime.setText(hmsTimeFormatter(millisUntilFinished));
+
+                progressBarCircle.setProgress((int) (millisUntilFinished / 1000));
+
+            }
+
+            @Override
+            public void onFinish() {
+
+                textViewTime.setText(hmsTimeFormatter(timeCountInMilliSeconds));
+                // call to initialize the progress bar values
+                setProgressBarValues();
+                // hiding the reset icon
+                imageViewReset.setVisibility(View.GONE);
+                // changing stop icon to start icon
+                imageViewStartStop.setImageResource(R.drawable.icon_start);
+                // making edit text editable
+                editTextMinute.setEnabled(true);
+                // changing the timer status to stopped
+                timerStatus = TimerStatus.STOPPED;
+            }
+
+        }.start();
+        countDownTimer.start();
+    }
+
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.activity_timer_fragment, null);
+        progressBarCircle = (ProgressBar) view.findViewById(R.id.progressBarCircle);
+        editTextMinute = (EditText) view.findViewById(R.id.editTextMinute);
+        textViewTime = (TextView) view.findViewById(R.id.textViewTime);
+        imageViewReset = (ImageView) view.findViewById(R.id.imageViewReset);
+        imageViewStartStop = (ImageView) view.findViewById(R.id.imageViewStartStop);
+
+        imageViewStartStop.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                switch (view.getId()){
+                    case R.id.imageViewReset:
+                        reset();
+                        break;
+                }
+            }
+        });
+
+        imageViewStartStop.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                switch (view.getId()){
+                    case R.id.imageViewStartStop:
+                        startStop();
+                        break;
+                }
+            }
+        });
+        return view;
+    }
+
 }
